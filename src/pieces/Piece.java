ac50b010c321eda3229635a1844e908ef1a7662d
kferte <qwerteek@gmail.com>
2018-11-15 15:39:01 +0100

    Add classes Board and Move, update calss Piece, add Enum Alliance.

--- src/pieces/Piece.java
+++ src/pieces/Piece.java
@@ -1,4 +1,20 @@
 package pieces;
 
-public class Piece {
+import Alliance;
+import board.Board;
+import board.Move;
+
+import java.util.List;
+
+public abstract class Piece {
+
+    protected final int piecePosition;
+    protected final Alliance pieceAllience;
+
+    Piece(final int piecePosition, final Alliance pieceAllience){
+        this.pieceAllience = pieceAllience;
+        this.piecePosition = piecePosition;
+    }
+
+    public abstract List<Move> calculateLegalMoves(final Board board);
 }
